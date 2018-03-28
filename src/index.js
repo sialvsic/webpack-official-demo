40be9dcbff4c7c9788bb7582a1a20394736803f5
zhou.yixing <zhou.yixing@rea-group.com>
2018-03-28 22:26:09 +0800

    Output Management

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.jpg';
 
+import printMe from './print';
+
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button')
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -15,6 +18,11 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
   },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    }),
+    new ManifestPlugin()
+  ],
   module: {
     rules: [
       {
