2132da793905961e7aa36515b509d0670619a1e5
zhou.yixing <zhou.yixing@rea-group.com>
2018-04-01 11:47:16 +0800

    Progressive Web Application

--- src/index.js
+++ src/index.js
@@ -72,3 +72,13 @@ function component3() {
 }
 
 document.body.appendChild(component3());
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
--- webpack.lib.config.js
+++ webpack.lib.config.js
@@ -1,16 +1,29 @@
 const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/trans.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'webpack-numbers.js',
+    filename: '[name].[hash:6].bundle.js',
     library: 'webpackNumbers',
     libraryTarget: 'umd'
   },
   plugins: [
-    // new BundleAnalyzerPlugin()
+    // new BundleAnalyzerPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Progressive Web Application'
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true
+    })
   ],
   externals: {
     lodash: {
@@ -19,6 +32,26 @@ module.exports = {
       amd: 'lodash',
       root: '_'
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   },
   mode: 'production'
 };
